8a58d323403c5ee4154922cee102ff528e5411da
Paolo Sciarra <itbhps@gmail.com>
2021-11-27 10:17:49 +0100

    added test

---
+++ core/src/test/kotlin/it/twinsbrain/funkt/CurriedTest.kt
@@ -0,0 +1,17 @@
+package it.twinsbrain.funkt
+
+import it.twinsbrain.funkt.functions.FunctionsExtensions.curried
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class CurriedTest {
+  private fun sum(x: Int, y: Int): Int = x + y
+
+  @Test
+  internal fun `curried working`() {
+    val sumCurried: (Int) -> (Int) -> Int = ::sum.curried()
+
+    assertThat(sum(2, 5)).isEqualTo(7)
+    assertThat(sumCurried(2)(5)).isEqualTo(7)
+  }
+}
